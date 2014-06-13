5360ca889fa7d25e1c5efcb6d29811b0fda0472b
Lucas Altman <lucas.altman@gmail.com>
2014-06-13 03:29:16 -0400

    Generate Blog controller with index action
    generated Blog Controller with index action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 RailsPersonal4511::Application.routes.draw do
+  get "blog/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
