d2da02e88e683e830edc35d67858ed3a5a5687db
Lucas Altman <lucas.altman@gmail.com>
2014-06-13 01:14:17 -0400

    initial commit after rails generation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsPersonal4511::Application.config.secret_key_base = '4fb8b72bcf78a91729d0be31d4eaeb52d857740274c243821c3fb9f0c47aecaf38e1986db78a03fe55d08d1a6ae42644f9ffb0de71337327c0af4ecd2f98ce5f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsPersonal4511::Application.config.session_store :cookie_store, key: '_rails_personal_4511_session'
