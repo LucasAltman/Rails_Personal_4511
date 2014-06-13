923b2b06110a64aae7729a7b0005d4304b539c13
Lucas Altman <lucas.altman@gmail.com>
2014-06-13 05:06:02 -0400

    Edited routes
    1) Made root direct to index action of blog controller
    2) set up initial routes for different pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 RailsPersonal4511::Application.routes.draw do
-  get "blog/index"
+  get '/blog', to: 'blog#index'
+
+  get '/designs', to: 'designs#index'
+
+  get '/code', to: 'code#index'
+
+  get '/contact' to: 'contact#index'
+
+
+
+
+  root 'blog#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
